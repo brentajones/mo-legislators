6faaaaa4a3e077e83434a734f3ace2909785a8b5
Brent Jones <bjones@stlpublicradio.org>
2016-02-26 11:15:59 -0600

    adds fed legislators

--- mo_legislators.py
+++ mo_legislators.py
@@ -19,3 +19,20 @@ with open('data/mo.csv', 'w+') as f:
             for var in list_vars:
                  row.append(get_val(var,var))
         w.writerow(row)
+
+mo_fed_legislators = mo_legislators.get_fed_legislators('MO')
+
+def get_val(var, val):
+        var = p.get(val, '')
+        return var
+
+with open('data/mo-fed.csv', 'w+') as f:
+    fieldnames = ['last_name', 'name_suffix', 'first_name', 'middle_name','party','title','district','website','twitter_id','youtube_id','facebook_id','bioguide_id']
+    w = csv.writer(f)
+    w.writerow(fieldnames)
+    for p in mo_fed_legislators:
+        row = []
+        if p['in_office'] == True:
+            for var in fieldnames:
+                 row.append(get_val(var,var))
+        w.writerow(row)
